0da4c211f38fde62eb09964e43bad628fab6e0d8
freeman <the.freeman.lab@gmail.com>
2016-01-20 02:21:21 -0500

    remove most tiles

--- index.js
+++ index.js
@@ -12,24 +12,16 @@ var base = function (start) {
   return {
     tiles: [
       {translation: [0, 0], paths: [0, 2, 4], cue: {fill: '#DE863A', scale: 1}},
-      {translation: [-1, 0], paths: [0, 4, 5], cue: {fill: '#00C3EE', scale: 1}},
-      {translation: [0, 1], paths: [2, 3, 4], target: {fill: '#FFFFFF'}},
-      {translation: [-1, 1], paths: [4, 5], cue: {fill: '#82C94A', scale: 1}},
-      {translation: [1, -1], paths: [2, 3]},
-      {translation: [1, 0], paths: [1, 3]},
-      {translation: [0, -1], paths: [1, 3, 5]},
-      {translation: [0, -2], paths: [0, 5]},
-      {translation: [1, -2], paths: [0, 2], cue: {fill: '#CF5557', scale: 1}}
+      {translation: [0, 1], paths: [2, 3, 4], target: {fill: '#FFFFFF'}}
     ],
     start: [{translation: start.translation, rotation: start.rotation}],
     target: [0, 1],
-    flash: ['#FF5050', '#FF8900', '#00C3EE', '#64FF00'],
-    message: 'welcome to hexaworld! try to find the big white circle'
+    flash: ['#FF5050', '#FF8900', '#00C3EE', '#64FF00']
   }
 }
 
 var maps = [
-  base({translation: [0, -2], rotation: 180}),
+  base({translation: [0, 0], rotation: 180}),
   base({translation: [1, -1], rotation: 0}),
   base({translation: [0, -1], rotation: 0})
 ]
